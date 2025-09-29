daf2456ae4d5560b5ec70ec33027691d577b98c7
Esraa Tabash <esraatabash.02@gmail.com>
2025-09-29 13:38:27 -0700

    chore: update screens and adapters (cleanup & fixes)

--- app/src/main/java/com/example/android2finalproject/models/OrderItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android2finalproject.models
-
-data class OrderItem(
-    var productId: String? = null,
-    var pName: String? = null,
-    var pPrice: Double? = null,
-    var imageUri: String? = null,
-    var quantity: Int? = null
-)
--- app/src/main/java/com/example/android2finalproject/models/Rating.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android2finalproject.models
-
-data class Rating(
-    var userId: String? = null,
-    var productId: String? = null,
-    var rating: Double? = null,
-    var comment: String? = null,
-    var createdAt: Long? = null
-)
